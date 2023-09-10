0be6e5a329aab9c302191c2280fd907208ec8955
suhail_yousaf <ysuhail444@gmail.com>
2023-09-10 22:15:30 +0530

    commit

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API = axios.create({ baseURL: "http://localhost:4000" })
+const API = axios.create({ baseURL: "https://adengage.onrender.com" })
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
