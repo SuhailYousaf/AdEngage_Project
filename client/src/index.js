16530704185b8076f19a154407be6480922257ae
suhail_yousaf <ysuhail444@gmail.com>
2023-09-10 18:44:16 +0530

    rendering issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Use the standard 'react-dom' package
 import './index.css';
 import App from './App';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux"
 import store from './redux/store.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-</React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API = axios.create({ baseURL: "https://adengage.onrender.com" })
+const API = axios.create({ baseURL: "http://localhost:4000"  })
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
